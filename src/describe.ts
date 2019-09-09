e1d680c30a6dc546d9d8ed38ca425c9395c687fb
Petri Lehtinen <petri.lehtinen@reaktor.com>
2019-09-09 10:37:13 +0300

    Describe

---
+++ src/describe.ts
@@ -0,0 +1,95 @@
+import { Either, left, right } from 'fp-ts/lib/Either'
+import { Client, QueryResult } from './pg'
+
+export async function describeStatement(
+  client: Client,
+  sql: string
+): Promise<Either<string, QueryResult<any>>> {
+  try {
+    return right(await client.query({ text: sql, describe: true }))
+  } catch (error) {
+    throw left(describeError(error, sql))
+  }
+}
+
+type PGError = {
+  message: string | undefined
+  severity: string | undefined
+  code: string | undefined
+  detail: string | undefined
+  hint: string | undefined
+  position: string | undefined
+  internalPosition: string | undefined
+  internalQuery: string | undefined
+  where: string | undefined
+  schema: string | undefined
+  table: string | undefined
+  column: string | undefined
+  dataType: string | undefined
+  constraint: string | undefined
+  file: string | undefined
+  line: string | undefined
+  routine: string | undefined
+}
+
+function describeError(error: PGError, sql: string): string {
+  const sourceLines = sql.split('\n')
+  const errorPos =
+    error.position && findPositionInFile(Number(error.position), sourceLines)
+  if (errorPos) {
+    return formatError(error, errorPos, sourceLines)
+  }
+  return error.message || ''
+}
+
+type PositionInFile = {
+  line: number
+  column: number
+}
+
+function findPositionInFile(
+  characterOffset: number,
+  sourceLines: string[]
+): PositionInFile | null {
+  type Acc = {
+    offset: number
+    lineNo: number
+    result: PositionInFile | null
+  }
+
+  const init: Acc = { offset: 0, lineNo: 1, result: null }
+  const { result } = sourceLines.reduce(
+    ({ offset, lineNo, result }, line) => ({
+      offset: offset + line.length + 1, // +1 for '\n' that was removed by .split('\n')
+      lineNo: lineNo + 1,
+      result:
+        offset <= characterOffset && characterOffset < offset + line.length + 1
+          ? { line: lineNo, column: characterOffset - offset }
+          : result,
+    }),
+    init
+  )
+
+  return result
+}
+
+function formatError(
+  error: PGError,
+  errorPos: PositionInFile,
+  sourceLines: string[]
+) {
+  const result = []
+  const lineNoStr = `LINE ${errorPos.line}: `
+  result.push(`${error.severity}:  ${error.message}`)
+  result.push(`${lineNoStr}${sourceLines[errorPos.line - 1]}`)
+  result.push(errorMarker(errorPos.column + lineNoStr.length))
+  if (error.hint) result.push(`HINT:  ${error.hint}`)
+  return result.join('\n')
+}
+
+function errorMarker(column: number): string {
+  const arr = []
+  for (let i = 0; i < column - 1; i++) arr.push(' ')
+  arr.push('^')
+  return arr.join('')
+}
