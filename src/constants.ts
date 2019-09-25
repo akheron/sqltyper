9ac615c1aad47249d757dc24ec6041a589e62ca4
Petri Lehtinen <petri.lehtinen@reaktor.com>
2019-09-25 09:56:53 +0300

    Add string functions and operators

--- src/constants.ts
+++ src/constants.ts
@@ -80,6 +80,7 @@ export const operators: Operator[] = [
   // name (upper case!), commutative, nullSafety
   op('OR', true, 'safe'),
   op('AND', true, 'safe'),
+  op('NOT', null, 'safe'),
   op('IS NULL', null, 'neverNull'),
   op('IS NOT NULL', null, 'neverNull'),
   op('<', false, 'safe'),
@@ -93,6 +94,7 @@ export const operators: Operator[] = [
   op('*', true, 'safe'),
   op('/', false, 'safe'),
   op('::', false, 'safe'),
+  op('||', false, 'safe'),
 ]
 
 // SQL functions
@@ -115,6 +117,58 @@ export const functions: Function[] = [
   func('count', 'neverNull'),
   func('sum', 'safe'),
   func('to_char', 'safe'),
+
+  // 9.4 String Functions and Operators
+  func('bit_length', 'safe'),
+  func('char_length', 'safe'),
+  func('character_length', 'safe'),
+  func('lower', 'safe'),
+  func('octet_length', 'safe'),
+  func('overlay', 'safe'), // TODO: special arg syntax
+  func('position', 'safe'), // TODO: special arg syntax
+  func('substring', 'safe'), // TODO: special arg syntax
+  func('trim', 'safe'), // TODO: special arg syntax
+  func('upper', 'safe'),
+  // ---
+  func('ascii', 'safe'),
+  func('btrim', 'safe'),
+  func('chr', 'safe'),
+  func('concat', 'neverNull'),
+  func('convert', 'safe'),
+  func('convert_from', 'safe'),
+  func('convert_to', 'safe'),
+  func('decode', 'safe'),
+  func('encode', 'safe'),
+  func('format', 'safe'), // TODO: NULL as 2nd parameter does not produce NULL
+  func('initcap', 'safe'),
+  func('left', 'safe'),
+  func('length', 'safe'),
+  func('lpad', 'safe'),
+  func('ltrim', 'safe'),
+  func('md5', 'safe'),
+  func('parse_ident', 'safe'),
+  func('pg_client_encoding', 'neverNull'),
+  func('quote_ident', 'safe'),
+  func('quote_literal', 'safe'),
+  func('quote_nullable', 'neverNull'),
+  func('regexp_match', 'safe'),
+  func('regexp_matches', 'safe'),
+  func('regexp_replace', 'safe'),
+  func('regexp_split_to_array', 'safe'),
+  func('regexp_split_to_table', 'safe'),
+  func('repeat', 'safe'),
+  func('replace', 'safe'),
+  func('reverse', 'safe'),
+  func('right', 'safe'),
+  func('rpad', 'safe'),
+  func('rtrim', 'safe'),
+  func('split_part', 'safe'),
+  func('strpos', 'safe'),
+  func('substr', 'safe'),
+  func('starts_with', 'safe'),
+  func('to_ascii', 'safe'),
+  func('to_hex', 'safe'),
+  func('translate', 'safe'),
 ]
 
 /// Helpers
