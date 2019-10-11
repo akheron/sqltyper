3b0d1ea1deca92da841560fd00083d5b80fc6ade
Petri Lehtinen <petri@digip.org>
2019-10-11 13:57:27 +0300

    Move FieldNullability to a separate file

--- src/infer.ts
+++ src/infer.ts
@@ -17,66 +17,12 @@ import {
   traverseATE,
 } from './fp-utils'
 import { functionNullSafety, operatorNullSafety } from './const-utils'
+import { FieldNullability } from './field-nullability'
 import { parse } from './parser'
 import { SchemaClient, Table, Column } from './schema'
 import { StatementDescription, StatementRowCount, ValueType } from './types'
 import * as Warn from './Warn'
 import * as InferM from './InferM'
-
-type FieldNullability = FieldNullability.Any | FieldNullability.Array
-
-namespace FieldNullability {
-  export type Any = { kind: 'Any'; nullable: boolean }
-  export type Array = {
-    kind: 'Array'
-    nullable: boolean
-    elemNullable: boolean
-  }
-
-  export function any(nullable: boolean): FieldNullability {
-    return { kind: 'Any', nullable }
-  }
-
-  export function array(
-    nullable: boolean,
-    elemNullable: boolean
-  ): FieldNullability {
-    return { kind: 'Array', nullable, elemNullable }
-  }
-
-  export function walk<T>(
-    nullability: FieldNullability,
-    handlers: {
-      any: (value: Any) => T
-      array: (value: Array) => T
-    }
-  ): T {
-    switch (nullability.kind) {
-      case 'Any':
-        return handlers.any(nullability)
-      case 'Array':
-        return handlers.array(nullability)
-    }
-  }
-
-  export const disjunction = (a: FieldNullability) => (b: FieldNullability) =>
-    walk(a, {
-      any: aAny =>
-        walk(b, {
-          any: bAny => any(aAny.nullable || bAny.nullable),
-          array: bArray => any(aAny.nullable || bArray.nullable),
-        }),
-      array: aArray =>
-        walk(b, {
-          any: bAny => any(aArray.nullable || bAny.nullable),
-          array: bArray =>
-            array(
-              aArray.nullable || bArray.nullable,
-              aArray.elemNullable || bArray.elemNullable
-            ),
-        }),
-    })
-}
 
 export type SourceColumn = {
   tableAlias: string
