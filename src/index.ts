4a9a26d0ad9b6b574a967f0d94f6e12ec51427f9
Simon Van Casteren <simon.van.casteren@gmail.com>
2021-06-03 11:39:33 +0200

    Added support for nested directories

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ import {
   generateTypeScript,
   validateStatement,
   TsModule,
+  TsModuleDir,
   generateIndexModule,
   CodegenTarget,
 } from './codegen'
@@ -78,6 +79,8 @@ export function generateTSCode(
 export { TsModule, TsModuleDir } from './codegen'
 
 export function indexModuleTS(
+  dirPath: string,
+  nestedDirs: TsModuleDir[],
   tsModules: TsModule[],
   options?: {
     prettierFileName?: string | null | undefined
@@ -85,7 +88,7 @@ export function indexModuleTS(
 ): Task.Task<string> {
   const { prettierFileName = null } = options || {}
   return pipe(
-    Task.of(generateIndexModule(tsModules)),
+    Task.of(generateIndexModule(dirPath, nestedDirs, tsModules)),
     Task.chain((tsCode) =>
       prettierFileName != null
         ? () => runPrettier(prettierFileName, tsCode)
