18d5a609fc6e6d6f80793d069130d7c31139e109
Petri Lehtinen <petri@digip.org>
2022-01-09 12:12:12 +0200

    More INSERT parsing tests

--- src-rs/parser/tests.rs
+++ src-rs/parser/tests.rs
@@ -11,12 +11,14 @@ fn assert_parse(input: &str) {
 
 #[test]
 fn test_insert() {
-    assert_parse("INSERT INTO person (id, age) VALUES ($1, $2), ($3, $4)");
+    assert_parse("INSERT INTO person DEFAULT VALUES");
+    assert_parse("INSERT INTO person (id, age) DEFAULT VALUES");
+    assert_parse("INSERT INTO person (id, age) VALUES (1, 2), (3, 4)");
     assert_parse("INSERT INTO person AS p VALUES (1, 2) ON CONFLICT DO NOTHING");
     assert_parse("INSERT INTO person VALUES (1, 2) ON CONFLICT ON CONSTRAINT constr DO NOTHING");
     assert_parse("INSERT INTO person VALUES (1, 2) ON CONFLICT (name) DO NOTHING");
     assert_parse("INSERT INTO person VALUES (1, 2) ON CONFLICT DO UPDATE SET age = 1, flag = true");
-    assert_parse("INSERT INTO person VALUES ($1, $2) RETURNING *");
-    assert_parse("INSERT INTO person VALUES ($1, $2) RETURNING id AS a, age - 42 b");
+    assert_parse("INSERT INTO person VALUES (1, 2) RETURNING *");
+    assert_parse("INSERT INTO person VALUES (1, 2) RETURNING id AS a, age - 1 b, flag");
     assert_parse("INSERT INTO person VALUES (1) ON CONFLICT DO NOTHING RETURNING *");
 }
