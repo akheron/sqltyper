c99357110bbf72ebea28e80063b3cefd489c4b60
Petri Lehtinen <petri@digip.org>
2022-02-15 17:07:33 +0200

    Add some info about operator and function null safety

--- src-rs/infer/null_safety.rs
+++ src-rs/infer/null_safety.rs
@@ -5,12 +5,44 @@ pub enum NullSafety {
     NeverNull,
 }
 
-pub fn operator_null_safety(_op: &str) -> NullSafety {
-    // TODO
-    NullSafety::Safe
+pub fn operator_null_safety(op: &str) -> NullSafety {
+    match op {
+        // FALSE AND NULL evaluates to FALSE, TRUE OR NULL evaluates to TRUE, so these operators
+        // ase unsafe. They're also not commutitave due to short-circuiting.
+        "AND" | "OR" => NullSafety::Unsafe,
+
+        "IS DISTINCT FROM"
+        | "IS NOT DISTINCT FROM"
+        | "IS NULL"
+        | "IS NOT NULL"
+        | "ISNULL"
+        | "NOTNULL"
+        | "IS TRUE"
+        | "IS NOT TRUE"
+        | "IS FALSE"
+        | "IS NOT FALSE"
+        | "IS UNKNOWN"
+        | "IS NOT UNKNOWN" => NullSafety::NeverNull,
+
+        _ => NullSafety::Safe,
+    }
 }
 
-pub fn builtin_function_null_safety(_func_name: &str) -> NullSafety {
-    // TODO
-    NullSafety::Safe
+pub fn builtin_function_null_safety(function_name: &str) -> NullSafety {
+    match function_name {
+        // 9.2. Comparison Functions and Operators
+        "num_nonnulls" | "num_nulls" => NullSafety::NeverNull,
+
+        // 9.3. Mathematical Functions and Operators
+        "pi" | "setseed" => NullSafety::NeverNull,
+
+        // 9.4. String Functions and Operators
+        "concat" | "concat_ws" | "pg_client_encoding" | "quote_nullable" => NullSafety::NeverNull,
+        "format" => NullSafety::Safe, // TODO: NULL as 2nd parameter does not produce NULL
+
+        // Not yet categorized
+        "daterange" | "now" | "count" => NullSafety::NeverNull,
+
+        _ => NullSafety::Safe,
+    }
 }
